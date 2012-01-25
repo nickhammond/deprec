cdf1b76b6ccf40bc326d74902116facf0e5a856c
Nick Hammond <nick@nickhammond.com>
2012-01-25 11:12:10 -0600

    debug for web_server_type_var

--- lib/deprec/recipes/passenger.rb
+++ lib/deprec/recipes/passenger.rb
@@ -32,6 +32,8 @@ Capistrano::Configuration.instance(:must_exist).load do
       task :install, :roles => :app do
         install_deps
         gem2.install 'passenger', passenger_version
+        puts "WEB SERVER TYPE IS #{web_server_type} #{web_server_type.class}"
+        exit
         if web_server_type == :nginx
           deprec2.nginx.install
         else
